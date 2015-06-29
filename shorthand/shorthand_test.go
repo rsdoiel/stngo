d2d9072b28893a9b993960555fad5aaf1f9084a4
R. S. Doiel <rsdoiel@gmail.com>
2015-06-28 21:10:03 -0700

    adding stubs

---
+++ shorthand/shorthand_test.go
@@ -0,0 +1,9 @@
+/**
+ * shorthand_test.go - tests for stngo package for handling shorthand definition and expantion.
+ * @author R. S. Doiel, <rsdoiel@gmail.com>
+ * copyright (c) 2015 all rights reserved.
+ * Released under the BSD 2-Clause license
+ */
+package shorthand
+
+// structs and methods go here.
