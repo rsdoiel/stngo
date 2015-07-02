95be969311e2af1e26f9333a4892d587b15ea08e
R. S. Doiel <rsdoiel@gmail.com>
2015-07-01 20:12:11 -0700

    snapshot

--- stn/stn_test.go
+++ stn/stn_test.go
@@ -15,9 +15,9 @@ import (
 )
 
 func ok(t *testing.T, expected bool, msg string) {
-		if expected != true {
-			t.Fatalf("Failed: " + msg)
-		}
+	if expected != true {
+		t.Fatalf("Failed: " + msg)
+	}
 }
 
 func TestIsDateLine(t *testing.T) {
@@ -25,37 +25,37 @@ func TestIsDateLine(t *testing.T) {
 	text := `2015-07-04`
 	expected := true
 	result := IsDateLine(text)
-	ok(t, expected == result, text + " is valid.")
+	ok(t, expected == result, text+" is valid.")
 
 	// IsDateLine expects dates in YYYYY-MM-DD format.
 	text = `January 20th, 2015`
 	expected = false
 	result = IsDateLine(text)
-	ok(t, expected == result, text + " is NOT valid.")
+	ok(t, expected == result, text+" is NOT valid.")
 
 	// Also not valid for IsDateLine...
 	text = `07-25-2015`
 	expected = false
 	result = IsDateLine(text)
-	ok(t, expected == result, text + " is NOT valid")
+	ok(t, expected == result, text+" is NOT valid")
 
 	// This is an entry not a DateLine
 	text = "08:00 - 9:00; misc; email and what not."
 	expected = false
 	result = IsDateLine(text)
-	ok(t, expected == result, text + " is NOT valid")
+	ok(t, expected == result, text+" is NOT valid")
 
 	// This is an empty line, not a DateLine
 	text = ""
 	expected = false
 	result = IsDateLine(text)
-	ok(t, expected == result, text + " is NOT valid")
+	ok(t, expected == result, text+" is NOT valid")
 
 	// This is just some random text, not a DateLine
 	text = "This is just some random text, not a DateLine"
 	expected = false
 	result = IsDateLine(text)
-	ok(t, expected == result, text + " is NOT valid")
+	ok(t, expected == result, text+" is NOT valid")
 }
 
 func TestParseDateLine(t *testing.T) {
@@ -63,37 +63,37 @@ func TestParseDateLine(t *testing.T) {
 	text := `2015-07-04`
 	expected := `2015-07-04`
 	result := ParseDateLine(text)
-	ok(t, expected == result, text + " is valid. Got: " + result)
+	ok(t, expected == result, text+" is valid. Got: "+result)
 
 	// IsDateLine expects dates in YYYYY-MM-DD format.
 	text = `January 20th, 2015`
 	expected = ""
 	result = ParseDateLine(text)
-	ok(t, expected == result, text + " is NOT valid.")
+	ok(t, expected == result, text+" is NOT valid.")
 
 	// Also not valid for IsDateLine...
 	text = `07-25-2015`
 	expected = ""
 	result = ParseDateLine(text)
-	ok(t, expected == result, text + " is NOT valid")
+	ok(t, expected == result, text+" is NOT valid")
 
 	// This is an entry not a DateLine
 	text = "08:00 - 9:00; misc; email and what not."
 	expected = ""
 	result = ParseDateLine(text)
-	ok(t, expected == result, text + " is NOT valid")
+	ok(t, expected == result, text+" is NOT valid")
 
 	// This is an empty line, not a DateLine
 	text = ""
 	expected = ""
 	result = ParseDateLine(text)
-	ok(t, expected == result, text + " is NOT valid")
+	ok(t, expected == result, text+" is NOT valid")
 
 	// This is just some random text, not a DateLine
 	text = "This is just some random text, not a DateLine"
 	expected = ""
 	result = ParseDateLine(text)
-	ok(t, expected == result, text + " is NOT valid")
+	ok(t, expected == result, text+" is NOT valid")
 }
 
 func TestIsEntry(t *testing.T) {
@@ -101,72 +101,76 @@ func TestIsEntry(t *testing.T) {
 	text := `2015-07-04`
 	expected := false
 	result := IsEntry(text)
-	ok(t, expected == result, text + " is valid.")
+	ok(t, expected == result, text+" is valid.")
 
 	// IsDateLine expects dates in YYYYY-MM-DD format.
 	text = `January 20th, 2015`
 	expected = false
 	result = IsEntry(text)
-	ok(t, expected == result, text + " is NOT valid.")
+	ok(t, expected == result, text+" is NOT valid.")
 
 	// Also not valid for IsDateLine...
 	text = `07-25-2015`
 	expected = false
 	result = IsEntry(text)
-	ok(t, expected == result, text + " is NOT valid")
+	ok(t, expected == result, text+" is NOT valid")
 
 	// This is an entry not a DateLine
 	text = "08:00 - 9:00; misc; email and what not."
 	expected = true
 	result = IsEntry(text)
-	ok(t, expected == result, text + " is valid, got: " + strconv.FormatBool(result))
+	ok(t, expected == result, text+" is valid, got: "+strconv.FormatBool(result))
 
 	// This is an empty line, not a DateLine
 	text = ""
 	expected = false
 	result = IsEntry(text)
-	ok(t, expected == result, text + " is NOT valid")
+	ok(t, expected == result, text+" is NOT valid")
 
 	// This is just some random text, not a DateLine
 	text = "This is just some random text, not a DateLine"
 	expected = false
 	result = IsEntry(text)
-	ok(t, expected == result, text + " is NOT valid")
+	ok(t, expected == result, text+" is NOT valid")
 }
 
 func TestParseEntry(t *testing.T) {
 	cells := splitCells("one; two; three")
-	ok(t, len(cells) == 3, "Should get three cells: " + strings.Join(cells, " | "))
+	ok(t, len(cells) == 3, "Should get three cells: "+strings.Join(cells, " | "))
 
 	activeDate := "2015-07-04"
 	// Simple text expected the dates in YYYY-MM-DD
+	// but this is not as an entry
 	text := `2015-07-04`
 	_, err := ParseEntry(activeDate, text)
-	ok(t, err != nil, text + " produced error on ParseEntry().")
+	ok(t, err != nil, text+" produced error on ParseEntry().")
 
-	// IsDateLine expects dates in YYYYY-MM-DD format.
+	// niether date nor entry
 	text = `January 20th, 2015`
 	_, err = ParseEntry(activeDate, text)
-	ok(t, err != nil, text + " produced error on ParseEntry().")
+	ok(t, err != nil, text+" produced error on ParseEntry().")
 
-	// Also not valid for IsDateLine...
+	// Also not valid IsDateLine/entry...
 	text = `07-25-2015`
 	_, err = ParseEntry(activeDate, text)
-	ok(t, err != nil, text + " produced error on ParseEntry().")
+	ok(t, err != nil, text+" produced error on ParseEntry().")
 
-	// This is an entry not a DateLine
-	text = "08:00 - 9:00; misc; email and what not."
-	_, err = ParseEntry(activeDate, text)
+	// This is an entry
+	text = "08:00 - 9:30; misc; email and what not."
+	entry, err := ParseEntry(activeDate, text)
 	ok(t, err == nil, fmt.Sprintf("%s  is Valid, got error: %q", text, err))
-	ok(t, false, "FIXME: Need to test Entry() return structure and make sure it converts nicely into String and JSON.")
+	ok(t, entry.Start.Hour() == 8, "should start at hoir of 8")
+	ok(t, entry.Start.Minute() == 0, "should have start minute zero")
+	ok(t, entry.End.Hour() == 9, "should end at hour of 8")
+	ok(t, entry.End.Minute() == 30, "should have end minute thirty")
 
 	// This is an empty line, not a DateLine
 	text = ""
 	_, err = ParseEntry(activeDate, text)
-	ok(t, err != nil, text + " produced error on ParseEntry().")
+	ok(t, err != nil, text+" produced error on ParseEntry().")
 
 	// This is just some random text, not a DateLine
 	text = "This is just some random text, not a DateLine"
 	_, err = ParseEntry(activeDate, text)
-	ok(t, err != nil, text + " produced error on ParseEntry().")
+	ok(t, err != nil, text+" produced error on ParseEntry().")
 }
