005945c400207cc8bd9a2dbb4e8b57f667fe8358
R. S. Doiel <rsdoiel@gmail.com>
2015-07-06 16:56:53 -0700

    snapshot

--- stn/report/report.go
+++ stn/report/report.go
@@ -9,10 +9,36 @@
 // Package report provides basic report rendering for Simple Timesheet Notation.
 package report
 
+import (
+    "../../stn"
+    "log"
+)
+
+// EntryAggregation
+type EntryAggregation []stn.Entry
+
+// Aggregate - add an entry to the EntryAggregate
+func (a *EntryAggregation) Aggregate(entry *stn.Entry) bool {
+    log.Fatal("EntryAggregation.Aggregate() not implemented.")
+    return false;
+}
+
 // Summarize - give the output of stnparse or stnfilter aggregate the results
 // by the first notation and the durration of time.
+func (e *EntryAggregation) Summarize() string {
+    log.Fatal("EntryAggregation.Summarize() not implemetned.")
+    return ""
+}
 
 // Detail - given the output of stnparse or stnfilter aggregate the
 // results by the first annotation preserving each second to N annotations as a narrative.
+func (e *EntryAggregation) Detail() string {
+    log.Fatal("EntryAggregation.Detail() not implemetned.")
+    return ""
+}
 
 // TotalDuration - given the output of stnfilter or stnparse calculate the total durration.
+func (e *EntryAggregation) TotalDurration() string {
+    log.Fatal("EntryAggregation.TotalDurration() not implemetned.")
+    return ""
+}
--- stn/report/report_test.go
+++ stn/report/report_test.go
@@ -9,8 +9,10 @@
 package report
 
 import (
+	"../../stn"
 	"log"
 	"testing"
+	"strings"
 )
 
 func ok(t *testing.T, expected bool, msg string) {
@@ -20,33 +22,29 @@ func ok(t *testing.T, expected bool, msg string) {
 }
 
 func TestAggregator(t *testing.T) {
-	log.Fatal("TestAggregator() not implemented.")
-}
-
-func TestHeadings(t *testing.T) {
-	log.Fatal("TestHeadings() not implemented.")
-}
-
-func TestFooter(t *testing.T) {
-	log.Fatal("TestFooter() not implemented.")
-}
-
-func TestBody(t *testing.T) {
-	log.Fatal("TestBody() not implemented.")
-}
-
-func TestReportsDaily(t *testing.T) {
-	log.Fatal("TestReportsDaily() not implemented.")
-}
-
-func TestReportsWeekly(t *testing.T) {
-	log.Fatal("TestReportsWeekly() not implemented.")
-}
-
-func TestReportsMonthly(t *testing.T) {
-	log.Fatal("TestReportsMonthly() not implemented.")
-}
-
-func TestReportsYearly(t *testing.T) {
-	log.Fatal("TestReportsMonthly() not implemented.")
-}
+	text := `2015-07-06T08:00:00-07:00	2015-07-06T08:30:00-07:00	misc	email, update basecamp
+2015-07-06T08:30:00-07:00	2015-07-06T11:00:00-07:00	ArchivesSpace	running through migration process, updating notes, testing
+2015-07-06T11:00:00-07:00	2015-07-06T11:45:00-07:00	misc	update Mac
+2015-07-03T08:00:00-07:00	2015-07-03T15:30:00-07:00	Holiday	4th of July observed
+2015-07-02T07:45:00-07:00	2015-07-02T09:30:00-07:00	misc	email, review stuff
+2015-07-02T09:30:00-07:00	2015-07-02T10:30:00-07:00	DLD meeting
+2015-07-02T10:30:00-07:00	2015-07-02T12:00:00-07:00	ArchivesSpace	running through migration process
+2015-07-02T03:00:00-07:00	2015-07-02T03:30:00-07:00	ArchivesSpace	Hangouts with Tommy to upgrade cls-arch.library.caltech.edu to v1.3.0, go over migration questions
+2015-07-01T07:45:00-07:00	2015-07-01T09:30:00-07:00	ArchivesSpace	continue reading docs, articles about approach and what problems are being solved.
+2015-07-01T09:30:00-07:00	2015-07-01T11:00:00-07:00	ArchivesSpace	meeting in SFL's MCR (3rd floot Multi-media Conference Room)`
+	aggregation := new(EntryAggregation)
+	entry := new(stn.Entry)
+	lines := strings.Split(text, "\n")
+	for i, line := range lines {
+		if entry.FromString(line) == true {
+			if aggregation.Aggregate(entry) != true {
+				log.Fatalf("Can't aggregate entry %d: %v", i, entry)
+			}
+		} else {
+			log.Fatalf("Can't read line no. %d: [%s]\n", i, line)
+		}
+	}
+	outText := aggregation.Summarize()
+	outLines := strings.Split(outText, "\n")
+	ok(t, len(outLines) > len(lines), "Report should have more lines than source: [" + outText + "]")
+ }
