091f42f1b43fe5b38a35760d5af5592430a81732
R. S. Doiel <rsdoiel@gmail.com>
2015-07-09 12:14:01 -0700

    Trimmed date from command line to YYYY-MM before settingup pipeline.

--- demo/rpt-time-by-month.sh
+++ demo/rpt-time-by-month.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 FOR_DATE=$(date +"%Y-%m")
 if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
-	echo "USAGE: rpt-time-by-month.sh YYYY-MM"
-	echo "    Without a date it reports the current week."
-	exit 1
+    echo "USAGE: rpt-time-by-month.sh YYYY-MM"
+    echo "    Without a date it reports the current week."
+    exit 1
 elif [ "$1" != "" ]; then
-	FOR_DATE="$1-01"
+    FOR_DATE="$1"
 fi
 
-END_OF_MONTH=$(reldate --from $FOR_DATE --end-of-month)
+START_OF_MONTH="${FOR_DATE:0:7}-01"
+END_OF_MONTH=$(reldate --from $START_OF_MONTH --end-of-month)
 
 # Now that we have date in the format needed, create a pipeline for the report.
-echo "Report for $FOR_DATE through $(reldate --from="$FOR_DATE" --end-of-month)"
+echo "Report for $START_OF_MONTH through $END_OF_MONTH"
 cat Time_Sheet.txt | shorthand -e "@now := $(date +%H:%M)" | stnparse |\
-    stnfilter -start "$FOR_DATE" -end "$END_OF_MONTH" | stnreport
+    stnfilter -start "$START_OF_MONTH" -end "$END_OF_MONTH" | stnreport
