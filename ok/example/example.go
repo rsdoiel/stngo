70eab9b7cbec9e8974e9068f10fae68b049ca225
R. S. Doiel <rsdoiel@gmail.com>
2016-12-24 15:26:27 -0800

    Quick Save

--- ok/example/example.go
+++
@@ -1,6 +0,0 @@
-package example
-
-// Hello returns a string with "Hello " concatenated with the value of s
-func Hello(s string) string {
-	return "Hello " + s
-}
--- stn_test.go
+++ stn_test.go
@@ -10,14 +10,10 @@
 package stn
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
 	"testing"
 	"time"
-
-	// stngo packages
-	"github.com/rsdoiel/stngo/ok"
 )
 
 func TestIsDateLine(t *testing.T) {
@@ -25,43 +21,57 @@ func TestIsDateLine(t *testing.T) {
 	text := `2015-07-04`
 	expected := true
 	result := IsDateLine(text)
-	ok.Ok(t, expected == result, text+" is valid.")
+	if expected != result {
+		t.Errorf("%q is valid.", text)
+	}
 
 	// Simple text expected the dates in MM/DD/YYYY
 	text = `07/04/2015`
 	expected = true
 	result = IsDateLine(text)
-	ok.Ok(t, expected == result, text+" is valid.")
+	if expected != result {
+		t.Errorf("%q is valid", text)
+	}
 
 	// IsDateLine expects dates in YYYYY-MM-DD format.
 	text = `January 20th, 2015`
 	expected = false
 	result = IsDateLine(text)
-	ok.Ok(t, expected == result, text+" is NOT valid.")
+	if expected != result {
+		t.Errorf("%q is NOT valid.", text)
+	}
 
 	// Also not valid for IsDateLine...
 	text = `07-25-2015`
 	expected = false
 	result = IsDateLine(text)
-	ok.Ok(t, expected == result, text+" is NOT valid")
+	if expected != result {
+		t.Errorf("%q is NOT valid.", text)
+	}
 
 	// This is an entry not a DateLine
 	text = "08:00 - 9:00; misc; email and what not."
 	expected = false
 	result = IsDateLine(text)
-	ok.Ok(t, expected == result, text+" is NOT valid")
+	if expected != result {
+		t.Errorf("%q is NOT valid.", text)
+	}
 
 	// This is an empty line, not a DateLine
 	text = ""
 	expected = false
 	result = IsDateLine(text)
-	ok.Ok(t, expected == result, text+" is NOT valid")
+	if expected != result {
+		t.Errorf("%q is NOT valid.", text)
+	}
 
 	// This is just some random text, not a DateLine
 	text = "This is just some random text, not a DateLine"
 	expected = false
 	result = IsDateLine(text)
-	ok.Ok(t, expected == result, text+" is NOT valid")
+	if expected != result {
+		t.Errorf("%q is NOT valid.", text)
+	}
 }
 
 func TestParseDateLine(t *testing.T) {
@@ -69,37 +79,49 @@ func TestParseDateLine(t *testing.T) {
 	text := `2015-07-04`
 	expected := `2015-07-04`
 	result := ParseDateLine(text)
-	ok.Ok(t, expected == result, text+" is valid. Got: "+result)
+	if expected != result {
+		t.Errorf("%q is valid. Get: %q", text, result)
+	}
 
 	// IsDateLine expects dates in YYYYY-MM-DD format.
 	text = `January 20th, 2015`
 	expected = ""
 	result = ParseDateLine(text)
-	ok.Ok(t, expected == result, text+" is NOT valid.")
+	if expected != result {
+		t.Errorf("%q is NOT valid.", text)
+	}
 
 	// Also not valid for IsDateLine...
 	text = `07-25-2015`
 	expected = ""
 	result = ParseDateLine(text)
-	ok.Ok(t, expected == result, text+" is NOT valid")
+	if expected != result {
+		t.Errorf("%q is NOT valid.", text)
+	}
 
 	// This is an entry not a DateLine
 	text = "08:00 - 9:00; misc; email and what not."
 	expected = ""
 	result = ParseDateLine(text)
-	ok.Ok(t, expected == result, text+" is NOT valid")
+	if expected != result {
+		t.Errorf("%q is NOT valid.", text)
+	}
 
 	// This is an empty line, not a DateLine
 	text = ""
 	expected = ""
 	result = ParseDateLine(text)
-	ok.Ok(t, expected == result, text+" is NOT valid")
+	if expected != result {
+		t.Errorf("%q is NOT valid.", text)
+	}
 
 	// This is just some random text, not a DateLine
 	text = "This is just some random text, not a DateLine"
 	expected = ""
 	result = ParseDateLine(text)
-	ok.Ok(t, expected == result, text+" is NOT valid")
+	if expected != result {
+		t.Errorf("%q is NOT valid.", text)
+	}
 }
 
 func TestIsEntry(t *testing.T) {
@@ -107,100 +129,148 @@ func TestIsEntry(t *testing.T) {
 	text := `2015-07-04`
 	expected := false
 	result := IsEntry(text)
-	ok.Ok(t, expected == result, text+" is valid.")
+	if expected != result {
+		t.Errorf("%q is valid.", text)
+	}
 
 	// IsDateLine expects dates in YYYYY-MM-DD format.
 	text = `January 20th, 2015`
 	expected = false
 	result = IsEntry(text)
-	ok.Ok(t, expected == result, text+" is NOT valid.")
+	if expected != result {
+		t.Errorf("%q is NOT valid.", text)
+	}
 
 	// Also not valid for IsDateLine...
 	text = `07-25-2015`
 	expected = false
 	result = IsEntry(text)
-	ok.Ok(t, expected == result, text+" is NOT valid")
+	if expected != result {
+		t.Errorf("%q is NOT valid.", text)
+	}
 
 	// This is an entry not a DateLine
 	text = "08:00 - 9:00; misc; email and what not."
 	expected = true
 	result = IsEntry(text)
-	ok.Ok(t, expected == result, text+" is valid, got: "+strconv.FormatBool(result))
+	if expected != result {
+		t.Errorf("%q is valid, got: %q", text, strconv.FormatBool(result))
+	}
 
 	// This is an empty line, not a DateLine
 	text = ""
 	expected = false
 	result = IsEntry(text)
-	ok.Ok(t, expected == result, text+" is NOT valid")
+	if expected != result {
+		t.Errorf("%q is NOT valid", text)
+	}
 
 	// This is just some random text, not a DateLine
 	text = "This is just some random text, not a DateLine"
 	expected = false
 	result = IsEntry(text)
-	ok.Ok(t, expected == result, text+" is NOT valid")
+	if expected != result {
+		t.Errorf("%q is NOT valid.", text)
+	}
 }
 
 func TestParseEntry(t *testing.T) {
 	cells := splitCells("one; two; three")
-	ok.Ok(t, len(cells) == 3, "Should get three cells: "+strings.Join(cells, " | "))
+	if len(cells) != 3 {
+		t.Errorf("Should get three cells: %q", strings.Join(cells, " | "))
+	}
 
 	activeDate := "2015-07-04"
 	// Simple text expected the dates in YYYY-MM-DD
 	// but this is not as an entry
 	text := `2015-07-04`
 	_, err := ParseEntry(activeDate, text)
-	ok.Ok(t, err != nil, text+" produced error on ParseEntry().")
+	if err != nil {
+		t.Errorf("%q produced error on ParseEntry() - %s.", text, err)
+	}
 
 	// niether date nor entry
 	text = `January 20th, 2015`
 	_, err = ParseEntry(activeDate, text)
-	ok.Ok(t, err != nil, text+" produced error on ParseEntry().")
+	if err == nil {
+		t.Errorf("%q produced no error on ParseEntry()", text)
+	}
 
 	// Also not valid IsDateLine/entry...
 	text = `07-25-2015`
 	_, err = ParseEntry(activeDate, text)
-	ok.Ok(t, err != nil, text+" produced error on ParseEntry().")
+	if err != nil {
+		t.Errorf("%q produced error on ParseEntry() - %s", text, err)
+	}
 
 	// This is an entry
 	text = "08:00 - 9:30; misc; email and what not."
 	entry, err := ParseEntry(activeDate, text)
-	ok.Ok(t, err == nil, fmt.Sprintf("%s  is Valid, got error: %q", text, err))
-	ok.Ok(t, entry.Start.Hour() == 8, "should start at hour of 8")
-	ok.Ok(t, entry.Start.Minute() == 0, "should have start minute 0")
-	ok.Ok(t, entry.End.Hour() == 9, "should end at hour of 9")
-	ok.Ok(t, entry.End.Minute() == 30, "should have end minute 30")
-	ok.Ok(t, len(entry.Annotations) == 2, "Should have two annoations")
+	if err != nil {
+		t.Errorf("%s is Valid, got error: %q", text, err)
+	}
+	if entry.Start.Hour() != 8 {
+		t.Errorf("should start at hour of 8, got %d", entry.Start.Hour())
+	}
+	if entry.Start.Minute() != 0 {
+		t.Errorf("should have start minute 0, %d", entry.Start.Minute())
+	}
+	if entry.End.Hour() != 9 {
+		t.Errorf("should end at hour of 9, got %d", entry.End.Hour())
+	}
+	if entry.End.Minute() != 30 {
+		t.Errorf("should have end minute 30, got %d", entry.End.Minute())
+	}
+	if len(entry.Annotations) != 2 {
+		t.Errorf("Should have two annoations, got %d", len(entry.Annotations))
+	}
 	if len(entry.Annotations) == 2 {
-		ok.Ok(t, entry.Annotations[0] == "misc", "first cell should be 'misc': ["+entry.Annotations[0]+"]")
-		ok.Ok(t, entry.Annotations[1] == "email and what not.", "first cell should be 'email and what not.': ["+entry.Annotations[1]+"]")
+		if entry.Annotations[0] != "misc" {
+			t.Errorf("first cell should be 'misc': [%q]", entry.Annotations[0])
+		}
+		if entry.Annotations[1] != "email and what not." {
+			t.Errorf("first cell should be 'email and what not.': [%q]", entry.Annotations[1])
+		}
 	}
 
 	jsonString := entry.JSON()
 	expectedString := `{"Start":"2015-07-04T08:00:00-07:00","End":"2015-07-04T09:30:00-07:00","Annotations":["misc","email and what not."]}`
-	ok.Ok(t, jsonString == expectedString, "entry.toJSON(): "+jsonString)
+	if jsonString != expectedString {
+		t.Errorf("entry.toJSON(): %s", jsonString)
+	}
 
 	text = entry.String()
 	expectedString = "2015-07-04T08:00:00-07:00\t2015-07-04T09:30:00-07:00\tmisc\temail and what not."
-	ok.Ok(t, text == expectedString, "entry.String(): "+text)
+	if text != expectedString {
+		t.Errorf("entry.String(): %q", text)
+	}
 
 	text = "08:22 - 1:34; afternoon; email and what not."
 	entry, err = ParseEntry(activeDate, text)
 	jsonString = entry.JSON()
 	expectedString = `{"Start":"2015-07-04T08:22:00-07:00","End":"2015-07-04T13:34:00-07:00","Annotations":["afternoon","email and what not."]}`
-	ok.Ok(t, jsonString == expectedString, "entry.toJSON(): "+jsonString)
+	if jsonString != expectedString {
+		t.Errorf("entry.toJSON(): %q", jsonString)
+	}
 	text = entry.String()
 	expectedString = "2015-07-04T08:22:00-07:00\t2015-07-04T13:34:00-07:00\tafternoon\temail and what not."
-	ok.Ok(t, text == expectedString, "entry.String(): "+text)
+	if text != expectedString {
+		t.Errorf("entry.String(): %q", text)
+	}
 
 	// This is an empty line, not a DateLine
 	text = ""
 	_, err = ParseEntry(activeDate, text)
-	ok.Ok(t, err != nil, text+" produced error on ParseEntry().")
+	if err == nil {
+		t.Errorf("%q produced no error on ParseEntry().", text)
+	}
 
 	// This is just some random text, not a DateLine
 	text = "This is just some random text, not a DateLine"
 	_, err = ParseEntry(activeDate, text)
-	ok.Ok(t, err != nil, text+" produced error on ParseEntry().")
+	if err == nil {
+		t.Errorf("%q produced error on ParseEntry().", text)
+	}
 }
 
 func TestFilter(t *testing.T) {
@@ -215,8 +285,9 @@ func TestFilter(t *testing.T) {
 	}
 	expected := true
 	result := t1.IsInRange(start, end)
-	ok.Ok(t, expected == result,
-		t1.String()+" is between "+start.String()+" and "+end.String())
+	if expected != result {
+		t.Errorf("%q is between %q and %q", t1.String(), start.String(), end.String())
+	}
 
 	s, _ = time.Parse("2006-01-02 15:04", "2015-06-04 08:38")
 	e, _ = time.Parse("2006-01-02 15:04", "2015-06-04 13:34")
@@ -224,29 +295,42 @@ func TestFilter(t *testing.T) {
 	t1.End = e
 	expected = false
 	result = t1.IsInRange(start, end)
-	ok.Ok(t, expected == result,
-		t1.String()+" not is between "+start.String()+" and "+end.String())
+	if expected != result {
+		t.Errorf("%q not is between %q and %q", t1.String(), start.String(), end.String())
+	}
 
 	expected = true
 	result = t1.IsMatch("one")
-	ok.Ok(t, expected == result, "one is an annotation")
+	if expected != result {
+		t.Errorf("one is an annotation %v, %v", expected, result)
+	}
 
 	expected = true
 	result = t1.IsMatch("two")
-	ok.Ok(t, expected == result, "one is an annotation")
+	if expected != result {
+		t.Errorf("one is an annotation, %v, %v", expected, result)
+	}
 
 	expected = false
 	result = t1.IsMatch("three")
-	ok.Ok(t, expected == result, "one is an annotation")
+	if expected != result {
+		t.Errorf("one is an annotation, %v, %v", expected, result)
+	}
 
 	t2 := new(Entry)
-	ok.Ok(t, t2.FromString(t1.String()) == true, "FromString should work")
-	ok.Ok(t, t1.Start == t2.Start, "Start should match")
-	ok.Ok(t, t1.End == t2.End, "End should match")
+	if t2.FromString(t1.String()) != true {
+		t.Errorf("FromString should work %v", t2.FromString(t1.String()))
+	}
+	if t1.Start != t2.Start {
+		t.Errorf("Start should match, %q == %q", t1.Start, t2.Start)
+	}
+	if t1.End != t2.End {
+		t.Errorf("End should match, %q == %q", t1.End, t2.End)
+	}
 	for i := 0; i < len(t1.Annotations); i++ {
-		ok.Ok(t, t1.Annotations[i] == t2.Annotations[i],
-			fmt.Sprintf("%s == %s failed\n",
-				t1.Annotations[i], t2.Annotations[i]))
-
+		if t1.Annotations[i] != t2.Annotations[i] {
+			t.Errorf("%s == %s failed\n",
+				t1.Annotations[i], t2.Annotations[i])
+		}
 	}
 }
