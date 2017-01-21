a89530908c86722323efa8c70c493c1c0078d318
R. S. Doiel <rsdoiel@gmail.com>
2017-01-21 00:08:50 -0800

    Quick Save

--- stn.go
+++ stn.go
@@ -19,7 +19,9 @@ import (
 )
 
 // Version of stn.go package.
-const Version = "v0.0.4"
+const (
+	Version = "v0.0.4"
+)
 
 var (
 	dateLineRE       = regexp.MustCompile("^[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9]$")
