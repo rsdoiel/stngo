2ac6d598860edfa8932e09eef11830fe8334292a
R. S. Doiel <rsdoiel@gmail>
2024-07-09 11:57:50 -0700

    fixed repo and package name alignment in Windows Powershell installer script

--- version.go
+++ version.go
@@ -12,7 +12,7 @@ const (
     ReleaseDate = "2024-07-09"
 
     // ReleaseHash, the Git hash when version.go was generated
-    ReleaseHash = "10f36ec"
+    ReleaseHash = "cb5ebaa"
 
     LicenseText = `
 Redistribution and use in source and binary forms, with or without
